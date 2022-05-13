50f13eebd09bb845ddbf690b3c90d7c0dd6e2624
Mike Chester <mikeachester@gmail.com>
2022-05-12 19:49:40 -0700

    update authenik tf, add auth for plex

--- provision/terraform/authentik/customization.tf
+++ provision/terraform/authentik/customization.tf
@@ -19,23 +19,51 @@ resource "authentik_policy_binding" "reputation-low" {
 }
 
 resource "authentik_policy_binding" "system-admins-apps" {
-  for_each = local.admin_apps
+  for_each = toset(
+    concat(
+      tolist(local.admin_apps),
+      tolist(concat(
+        tolist(local.media_apps),
+        tolist(["paperless"])
+      ))
+    )
+  )
 
   target = authentik_application.name[each.key].uuid
-  group = authentik_group.system-admins.id
-  order  = 10
+  group  = authentik_group.system-admins.id
+  order  = 5
 }
 
 resource "authentik_policy_binding" "media-users-apps" {
   for_each = local.media_apps
 
   target = authentik_application.name[each.key].uuid
-  group = authentik_group.media-users.id
+  group  = authentik_group.media-users.id
   order  = 10
 }
 
 resource "authentik_policy_binding" "paperless-users-app" {
   target = authentik_application.name["paperless"].uuid
-  group = authentik_group.paperless-users.id
+  group  = authentik_group.paperless-users.id
   order  = 10
+}
+
+resource "authentik_scope_mapping" "x-plex-token" {
+  name       = "x-plex-token"
+  scope_name = "x-plex-token"
+  expression = <<EOF
+from authentik.sources.plex.models import PlexSourceConnection
+connection = PlexSourceConnection.objects.filter(user=request.user).first()
+if not connection:
+    return {}
+return {
+    "ak_proxy": {
+        "user_attributes": {
+            "additionalHeaders": {
+                "X-Plex-Token": connection.plex_token
+            }
+        }
+    }
+}
+EOF
 }
