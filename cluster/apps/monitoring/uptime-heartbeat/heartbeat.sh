25135e7c5276cda945051c0612784a8c73143f63
Mike Chester <mike@chester.io>
2022-11-06 11:54:56 -0800

    fix(heartbeat): fix script

--- cluster/apps/monitoring/uptime-heartbeat/heartbeat.sh
+++ cluster/apps/monitoring/uptime-heartbeat/heartbeat.sh
@@ -5,11 +5,4 @@ if [[ -z "${HEARTBEAT_URL}" ]]; then
     exit 0
 fi
 
-status_code=$(curl --connect-timeout 10 --max-time 30 -I -s -o /dev/null -w '%{http_code}' "${HEARTBEAT_URL}")
-if [[ ! ${status_code} =~ ^[2|3][0-9]{2}$ ]]; then
-    printf "%s - Error - Heartbeat request failed, http code: %s" "$(date -u)" "${status_code}"
-    exit 0
-fi
-
-printf "%s - Ok - Heartbeat request received and processed successfully" "$(date -u)"
-exit 0
+wget -qS -T 10 -O /dev/null  "${HEARTBEAT_URL}"
