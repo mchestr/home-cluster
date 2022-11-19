2718f2b92bb162df471b02fc5c03d4fb9c0e939d
Mike Chester <mike@chester.io>
2022-11-18 22:25:50 -0800

    feat: add authentik tf

--- provision/terraform/authentik/main.tf
+++ provision/terraform/authentik/main.tf
@@ -1,22 +1,67 @@
 terraform {
 
+  required_version = ">= 1.3.0"
+  cloud {
+    hostname     = "app.terraform.io"
+    organization = "mchestr"
+
+    workspaces {
+      name = "home-authentik"
+    }
+  }
+
   required_providers {
     authentik = {
       source  = "goauthentik/authentik"
       version = "2022.10.0"
     }
-    sops = {
-      source  = "carlpett/sops"
-      version = "0.7.1"
-    }
   }
 }
 
-data "sops_file" "authentik_secrets" {
-  source_file = "secret.sops.yaml"
-}
+locals {
+  proxy_apps = {
+    alert-manager       = {}
+    appdaemon           = { group = "Home Automation" }
+    appdaemon-code      = { group = "Code Editors" }
+    bazarr              = { group = "Media", basic_auth_enabled = true }
+    cal                 = {}
+    dashboard           = {}
+    emqx                = { basic_auth_enabled = true }
+    esphome             = { group = "Home Automation" }
+    home-assistant-code = { group = "Code Editors" }
+    hyperion-ng         = { group = "Home" }
+    kopia               = {}
+    lidarr              = { group = "Media", basic_auth_enabled = true }
+    longhorn            = {}
+    paperless           = { group = "Home" }
+    prometheus          = {}
+    prowlarr            = { group = "Media", basic_auth_enabled = true }
+    qbittorrent         = { group = "Downloaders", basic_auth_enabled = true }
+    radarr              = { group = "Media", basic_auth_enabled = true }
+    radarr-4k           = { group = "Media", basic_auth_enabled = true }
+    sabnzbd             = { group = "Downloaders" }
+    sonarr              = { group = "Media", basic_auth_enabled = true }
+    sync                = { group = "Home", basic_auth_enabled = true }
+    tautulli            = { group = "Media" }
+    thanos              = {}
+    traefik             = {}
+    zigbee2mqtt         = { group = "Home Automation" }
+    zwavejs2mqtt        = { group = "Home Automation" }
+  }
 
-provider "authentik" {
-  url   = data.sops_file.authentik_secrets.data["authentik_url"]
-  token = data.sops_file.authentik_secrets.data["authentik_token"]
+  oauth2_apps = {
+    grafana = {}
+    minio   = { extra_scopes = [authentik_scope_mapping.oidc-scope-minio.id] }
+    outline = { }
+  }
+
+  ldap_apps = {
+  }
+
+  media_apps = {
+    for k, v in authentik_application.name : k => v if contains(["Media", "Downloaders"], v.group)
+  }
+  home_apps = {
+    for k, v in authentik_application.name : k => v if contains(["Home"], v.group)
+  }
 }
