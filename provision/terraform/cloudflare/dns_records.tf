3bfc0bc43bfc3ab745df16ca08387d9de9b53b31
Mike Chester <mike@chester.io>
2022-12-04 00:58:13 -0800

    feat(tf): add aws ses values

--- provision/terraform/cloudflare/dns_records.tf
+++ provision/terraform/cloudflare/dns_records.tf
@@ -15,3 +15,43 @@ resource "cloudflare_record" "root" {
   type    = "CNAME"
   ttl     = 1
 }
+
+# Amazon SES Settings
+resource "cloudflare_record" "aws_ses_cname1" {
+  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  proxied = false
+  type    = "CNAME"
+  name    = var.aws_ses_cname1
+  value   = var.aws_ses_cname1_value
+}
+resource "cloudflare_record" "aws_ses_cname2" {
+  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  proxied = false
+  type    = "CNAME"
+  name    = var.aws_ses_cname2
+  value   = var.aws_ses_cname2_value
+}
+resource "cloudflare_record" "aws_ses_cname2" {
+  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  proxied = false
+  type    = "CNAME"
+  name    = var.aws_ses_cname2
+  value   = var.aws_ses_cname2_value
+}
+resource "cloudflare_record" "aws_ses_mx" {
+  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  proxied = false
+  type    = "MX"
+  name    = var.aws_ses_mx
+  value   = "feedback-smtp.us-west-2.amazonses.com"
+}
+resource "cloudflare_record" "aws_ses_txt" {
+  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  proxied = false
+  type    = "TXT"
+  name    = var.aws_ses_txt
+  value   = "v=spf1 include:amazonses.com ~all"
+  data {
+    priority = 10
+  }
+}
