f0f4bf9022ab1f6503e268f115a1ae2bc6d7c0c3
Mike Chester <mike@chester.io>
2022-12-03 23:40:58 -0800

    feat: update cf ruleset

--- provision/terraform/cloudflare/ruleset.tf
+++ provision/terraform/cloudflare/ruleset.tf
@@ -2,16 +2,16 @@ resource "cloudflare_ruleset" "transform_remove_x_forward_for" {
   zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
   name    = "Remove X-Forwarded-For Header"
   kind    = "zone"
-  phase   = "http_request_transform"
+  phase   = "http_request_late_transform"
   rules {
-    enabled    = true
-    action     = "rewrite"
-    expression = "(http.x_forwarded_for ne \"\")"
+    action = "rewrite"
     action_parameters {
       headers {
         name      = "X-Forwarded-For"
         operation = "remove"
       }
     }
+    enabled    = true
+    expression = "true"
   }
 }
