9736709e621fe53990a6aa01f94d9e3defc8bcfb
Mike Chester <mike@chester.io>
2022-12-04 00:58:13 -0800

    feat(tf): add aws ses values

--- provision/terraform/cloudflare/variables.tf
+++ provision/terraform/cloudflare/variables.tf
@@ -10,3 +10,35 @@ variable "cloudflare_domain" {
   type        = string
   description = "Cluster domain"
 }
+variable "aws_ses_cname1" {
+  type        = string
+  description = "AWS SES CNAME 1"
+}
+variable "aws_ses_cname1_value" {
+  type        = string
+  description = "AWS SES CNAME 1 value"
+}
+variable "aws_ses_cname2" {
+  type        = string
+  description = "AWS SES CNAME 2"
+}
+variable "aws_ses_cname2_value" {
+  type        = string
+  description = "AWS SES CNAME 2 value"
+}
+variable "aws_ses_cname3" {
+  type        = string
+  description = "AWS SES CNAME 3"
+}
+variable "aws_ses_cname3_value" {
+  type        = string
+  description = "AWS SES CNAME 3 value"
+}
+variable "aws_ses_mx" {
+  type        = string
+  description = "AWS SES MX record"
+}
+variable "aws_ses_txt" {
+  type        = string
+  description = "AWS SES TXT record"
+}
