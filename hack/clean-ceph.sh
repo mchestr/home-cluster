c2876feac4fe161929e0425607e99f2b274e30ce
Mike Chester <mike@chester.io>
2022-12-18 15:54:49 -0800

    feat(talos): swap to talos and cry into pillow
    https://www.youtube.com/watch?v=QUAItQmq-LU

---
+++ hack/clean-ceph.sh
@@ -0,0 +1,165 @@
+#!/bin/bash
+
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Pod
+metadata:
+  name: disk-clean-k8s-0
+spec:
+  restartPolicy: Never
+  nodeName: k8s-0
+  volumes:
+  - name: rook-data-dir
+    hostPath:
+      path: /var/lib/rook
+  containers:
+  - name: disk-clean
+    image: busybox
+    securityContext:
+      privileged: true
+    volumeMounts:
+    - name: rook-data-dir
+      mountPath: /node/rook-data
+    command: ["/bin/sh", "-c", "rm -rf /node/rook-data/*"]
+EOF
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Pod
+metadata:
+  name: disk-clean-k8s-1
+spec:
+  restartPolicy: Never
+  nodeName: k8s-1
+  volumes:
+  - name: rook-data-dir
+    hostPath:
+      path: /var/lib/rook
+  containers:
+  - name: disk-clean
+    image: busybox
+    securityContext:
+      privileged: true
+    volumeMounts:
+    - name: rook-data-dir
+      mountPath: /node/rook-data
+    command: ["/bin/sh", "-c", "rm -rf /node/rook-data/*"]
+EOF
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Pod
+metadata:
+  name: disk-clean-k8s-2
+spec:
+  restartPolicy: Never
+  nodeName: k8s-2
+  volumes:
+  - name: rook-data-dir
+    hostPath:
+      path: /var/lib/rook
+  containers:
+  - name: disk-clean
+    image: busybox
+    securityContext:
+      privileged: true
+    volumeMounts:
+    - name: rook-data-dir
+      mountPath: /node/rook-data
+    command: ["/bin/sh", "-c", "rm -rf /node/rook-data/*"]
+EOF
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Pod
+metadata:
+  name: disk-clean-k8s-3
+spec:
+  restartPolicy: Never
+  nodeName: k8s-3
+  volumes:
+  - name: rook-data-dir
+    hostPath:
+      path: /var/lib/rook
+  containers:
+  - name: disk-clean
+    image: busybox
+    securityContext:
+      privileged: true
+    volumeMounts:
+    - name: rook-data-dir
+      mountPath: /node/rook-data
+    command: ["/bin/sh", "-c", "rm -rf /node/rook-data/*"]
+EOF
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Pod
+metadata:
+  name: disk-wipe-k8s-0
+spec:
+  restartPolicy: Never
+  nodeName: k8s-0
+  containers:
+  - name: disk-wipe
+    image: busybox
+    securityContext:
+      privileged: true
+    command: ["/bin/sh", "-c", "dd if=/dev/zero bs=1M count=100 oflag=direct of=/dev/sdb"]
+EOF
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Pod
+metadata:
+  name: disk-wipe-k8s-1
+spec:
+  restartPolicy: Never
+  nodeName: k8s-1
+  containers:
+  - name: disk-wipe
+    image: busybox
+    securityContext:
+      privileged: true
+    command: ["/bin/sh", "-c", "dd if=/dev/zero bs=1M count=100 oflag=direct of=/dev/nvme0n1"]
+EOF
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Pod
+metadata:
+  name: disk-wipe-k8s-2
+spec:
+  restartPolicy: Never
+  nodeName: k8s-2
+  containers:
+  - name: disk-wipe
+    image: busybox
+    securityContext:
+      privileged: true
+    command: ["/bin/sh", "-c", "dd if=/dev/zero bs=1M count=100 oflag=direct of=/dev/sda"]
+EOF
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Pod
+metadata:
+  name: disk-wipe-k8s-2-2
+spec:
+  restartPolicy: Never
+  nodeName: k8s-2
+  containers:
+  - name: disk-wipe
+    image: busybox
+    securityContext:
+      privileged: true
+    command: ["/bin/sh", "-c", "dd if=/dev/zero bs=1M count=100 oflag=direct of=/dev/sdc"]
+EOF
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Pod
+metadata:
+  name: disk-wipe-k8s-3
+spec:
+  restartPolicy: Never
+  nodeName: k8s-3
+  containers:
+  - name: disk-wipe
+    image: busybox
+    securityContext:
+      privileged: true
+    command: ["/bin/sh", "-c", "dd if=/dev/zero bs=1M count=100 oflag=direct of=/dev/sdb"]
+EOF
---
+++ hack/kopia-restore.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# task snapshot:restore APP=vaultwarden CLAIM=vaultwarden-config-v1
+task snapshot:restore APP=appdaemon CLAIM=appdaemon-config-v1
+task snapshot:restore APP=esphome CLAIM=esphome-config-v1
+task snapshot:restore APP=home-assistant CLAIM=home-assistant-config-v1
+task snapshot:restore APP=radicale CLAIM=radicale-data-v1
+task snapshot:restore APP=unifi CLAIM=unifi-data-v1
+task snapshot:restore APP=zigbee2mqtt CLAIM=zigbee2mqtt-config-v1
+task snapshot:restore APP=zwavejs2mqtt CLAIM=zwavejs2mqtt-config-v1
+task snapshot:restore APP=bazarr CLAIM=bazarr-config-v1
+task snapshot:restore APP=lidarr CLAIM=lidarr-config-v1
+task snapshot:restore APP=overseerr CLAIM=overseerr-config-v1
+task snapshot:restore APP=plex CLAIM=plex-config-v1
+task snapshot:restore APP=prowlarr CLAIM=prowlarr-config-v1
+task snapshot:restore APP=qbittorrent CLAIM=qbittorrent-config-v1
+task snapshot:restore APP=radarr-4k CLAIM=radarr-4k-config-v1
+task snapshot:restore APP=radarr CLAIM=radarr-config-v1
+task snapshot:restore APP=sabnzbd CLAIM=sabnzbd-config-v1
+task snapshot:restore APP=sonarr CLAIM=sonarr-config-v1
+task snapshot:restore APP=tautulli CLAIM=tautulli-config-v1
+task snapshot:restore APP=uptime-kuma CLAIM=uptime-kuma-config-v1
