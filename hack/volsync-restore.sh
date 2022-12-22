3e02e078a1e8be94fc194e09c0b5af211b249953
Mike Chester <mike@chester.io>
2022-12-22 10:54:46 -0800

    fix ns

--- hack/volsync-restore.sh
+++ hack/volsync-restore.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
 task volsync:restore RSRC=vaultwarden CLAIM=vaultwarden-config-v1
-task volsync:restore RSRC=appdaemon CLAIM=appdaemon-config-v1 NAMESPACE=home
-task volsync:restore RSRC=esphome CLAIM=esphome-config-v1 NAMESPACE=home
-task volsync:restore RSRC=home-assistant CLAIM=home-assistant-config-v1 NAMESPACE=home
-task volsync:restore RSRC=zigbee2mqtt CLAIM=zigbee2mqtt-config-v1 NAMESPACE=home
-task volsync:restore RSRC=zwavejs2mqtt CLAIM=zwavejs2mqtt-config-v1 NAMESPACE=home
-task volsync:restore RSRC=radicale CLAIM=radicale-data-v1 NAMESPACE=home
-task volsync:restore RSRC=unifi CLAIM=unifi-data-v1 NAMESPACE=home
-task volsync:restore RSRC=zigbee2mqtt CLAIM=zigbee2mqtt-config-v1 NAMESPACE=home
-task volsync:restore RSRC=zwavejs2mqtt CLAIM=zwavejs2mqtt-config-v1 NAMESPACE=home
+task volsync:restore RSRC=appdaemon CLAIM=appdaemon-config-v1
+task volsync:restore RSRC=esphome CLAIM=esphome-config-v1
+task volsync:restore RSRC=zigbee2mqtt CLAIM=zigbee2mqtt-config-v1
+task volsync:restore RSRC=radicale CLAIM=radicale-data-v1
+task volsync:restore RSRC=unifi CLAIM=unifi-data-v1
 task volsync:restore RSRC=bazarr CLAIM=bazarr-config-v1 NAMESPACE=media
 task volsync:restore RSRC=lidarr CLAIM=lidarr-config-v1 NAMESPACE=media
 task volsync:restore RSRC=overseerr CLAIM=overseerr-config-v1 NAMESPACE=media
