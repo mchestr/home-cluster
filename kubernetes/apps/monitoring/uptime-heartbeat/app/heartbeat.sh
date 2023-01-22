b05e6bb4a55350749bd3d7c08884d70c7194d62d
Mike Chester <mike@chester.io>
2023-01-22 10:36:44 -0800

    Remove heartbeat in favor of alertmanager heartbeat

--- kubernetes/apps/monitoring/uptime-heartbeat/app/heartbeat.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [[ -z "${HEARTBEAT_URL}" ]]; then
-    printf "%s - Error - Missing HEARTBEAT_URL environment variable" "$(date -u)"
-    exit 0
-fi
-
-wget -qS -T 10 -O /dev/null  "${HEARTBEAT_URL}"
