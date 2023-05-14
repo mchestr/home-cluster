358fd9c63ae398f2dbf3e17afa6a20c4d528376e
Mike Chester <mike@chester.io>
2023-05-14 16:52:28 -0700

    chore: simplify settings

--- terraform/cloudflare/tunnel.tf
+++ terraform/cloudflare/tunnel.tf
@@ -3,3 +3,11 @@ resource "cloudflare_tunnel" "k8s" {
   name       = "k8s"
   secret     = var.cloudflared_tunnel_secret
 }
+
+resource "cloudflare_record" "status" {
+  zone_id = data.cloudflare_zone.domain.id
+  name    = "ingress"
+  value   = "${cloudflare_tunnel.k8s.id}.cfargotunnel.com"
+  type    = "CNAME"
+  proxied = true
+}
