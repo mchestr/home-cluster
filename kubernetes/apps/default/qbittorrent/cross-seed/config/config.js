54ee8d90431f2c9d9321a3cbf2758b3f4602fa6f
Mike Chester <mike@chester.io>
2023-05-31 19:55:13 -0700

    feat: try cross seed maybe

---
+++ kubernetes/apps/default/qbittorrent/cross-seed/config/config.js
@@ -0,0 +1,20 @@
+odule.exports = {
+    delay: 30,
+
+    torznab: [
+      "http://prowlarr.default.svc.cluster.local:9696/25/api?apikey={{ .PROWLARR_API_KEY }}",
+      "http://prowlarr.default.svc.cluster.local:9696/31/api?apikey={{ .PROWLARR_API_KEY }}",
+      "http://prowlarr.default.svc.cluster.local:9696/30/api?apikey={{ .PROWLARR_API_KEY }}",
+    ],
+
+    action: "inject",
+    matchMode: "safe",
+    skipRecheck: true,
+    includeEpisodes: true,
+    includeNonVideos: true,
+    duplicateCategories: true,
+    outputDir: "/cross-seeds",
+    torrentDir: "/config/qBittorrent/BT_backup",
+    qbittorrentUrl: "http://qbittorrent.default.svc.cluster.local:8080",
+    rssCadence: "15 minutes", // autobrr doesnt get every announcement
+};
