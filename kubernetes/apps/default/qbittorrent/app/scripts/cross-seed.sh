790bcbcca0d5831e7cd170ba3f60b0e22dd14803
Mike Chester <mike@chester.io>
2023-06-22 18:08:05 -0700

    feat: enable crossseed

--- kubernetes/apps/default/qbittorrent/app/scripts/cross-seed.sh
+++ kubernetes/apps/default/qbittorrent/app/scripts/cross-seed.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# /usr/bin/curl -X POST \
-#     --data-urlencode "infoHash=$1" \
-#     http://localhost:2468/api/webhook
+/usr/bin/curl -X POST \
+    --data-urlencode "infoHash=$1" \
+    http://localhost:2468/api/webhook
