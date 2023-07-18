f84012641a3c18041416cd39b92070a4292fceaa
mchesterbot <202745+mchesterbot[bot]@users.noreply.github.com>
2023-07-18 21:14:56 +0000

    feat(terraform): update terraform google to v4.74.0

--- terraform/google-cloud/main.tf
+++ terraform/google-cloud/main.tf
@@ -13,7 +13,7 @@ terraform {
   required_providers {
     google = {
         source = "hashicorp/google"
-        version = "4.73.2"
+        version = "4.74.0"
     }
     cloudflare = {
       source  = "cloudflare/cloudflare"
