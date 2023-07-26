e1d5794b5b15f321094c8077a342ba1ad532d224
mchesterbot <202745+mchesterbot[bot]@users.noreply.github.com>
2023-07-26 04:20:45 +0000

    feat(terraform): update terraform cloudflare to v4.11.0

--- terraform/google-cloud/main.tf
+++ terraform/google-cloud/main.tf
@@ -17,7 +17,7 @@ terraform {
     }
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "4.10.0"
+      version = "4.11.0"
     }
     random = {
         source = "hashicorp/random"
