5a4413fb3fde09ebfd3254bce3f53884cac2c9c6
mchesterbot <202745+mchesterbot[bot]@users.noreply.github.com>
2023-09-06 01:13:49 +0000

    feat(terraform): update terraform cloudflare to v4.14.0

--- terraform/cloudflare/main.tf
+++ terraform/cloudflare/main.tf
@@ -13,7 +13,7 @@ terraform {
   required_providers {
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "4.13.0"
+      version = "4.14.0"
     }
     onepassword = {
       source = "1Password/onepassword"
