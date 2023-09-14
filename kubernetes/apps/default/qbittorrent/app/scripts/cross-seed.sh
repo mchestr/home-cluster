639f3c096651e1bb9aa0a0759d7f91cbefec7f6e
Mike Chester <mike@chester.io>
2023-09-13 18:31:55 -0700

    fix: update complete script

--- kubernetes/apps/default/qbittorrent/app/scripts/cross-seed.sh
+++ kubernetes/apps/default/qbittorrent/app/scripts/cross-seed.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
-/usr/bin/curl -X POST \
-    --data-urlencode "infoHash=$1" \
-    http://localhost:2468/api/webhook
+# qBittorrent settings > 'Run external program on torrent finished'
+# /scripts/completed.sh "%F"
+/bin/chmod -R 750 "$1"
+/usr/bin/curl --silent --request POST --data-urlencode "path=$1" http://localhost:2468/api/webhook
