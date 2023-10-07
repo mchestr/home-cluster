921a64bfb80cb6daf6d3e612dc4c2f7cbd93d63a
Mike Chester <mike@chester.io>
2023-10-06 20:09:54 -0700

    chore: mass reformat, what can go wrong

--- kubernetes/apps/default/qbittorrent/app/config/config.js
+++ kubernetes/apps/default/qbittorrent/app/config/config.js
@@ -1,23 +1,23 @@
 module.exports = {
-    delay: 30,
+  delay: 30,
 
-    torznab: [
-      "http://prowlarr.default.svc.cluster.local:9696/25/api?apikey={{ .PROWLARR_API_KEY }}",
-      "http://prowlarr.default.svc.cluster.local:9696/30/api?apikey={{ .PROWLARR_API_KEY }}",
-      "http://prowlarr.default.svc.cluster.local:9696/31/api?apikey={{ .PROWLARR_API_KEY }}",
-      "http://prowlarr.default.svc.cluster.local:9696/33/api?apikey={{ .PROWLARR_API_KEY }}",
-      "http://prowlarr.default.svc.cluster.local:9696/34/api?apikey={{ .PROWLARR_API_KEY }}",
-      "http://prowlarr.default.svc.cluster.local:9696/35/api?apikey={{ .PROWLARR_API_KEY }}",
-    ],
+  torznab: [
+    "http://prowlarr.default.svc.cluster.local:9696/25/api?apikey={{ .PROWLARR_API_KEY }}",
+    "http://prowlarr.default.svc.cluster.local:9696/30/api?apikey={{ .PROWLARR_API_KEY }}",
+    "http://prowlarr.default.svc.cluster.local:9696/31/api?apikey={{ .PROWLARR_API_KEY }}",
+    "http://prowlarr.default.svc.cluster.local:9696/33/api?apikey={{ .PROWLARR_API_KEY }}",
+    "http://prowlarr.default.svc.cluster.local:9696/34/api?apikey={{ .PROWLARR_API_KEY }}",
+    "http://prowlarr.default.svc.cluster.local:9696/35/api?apikey={{ .PROWLARR_API_KEY }}",
+  ],
 
-    action: "inject",
-    matchMode: "safe",
-    skipRecheck: true,
-    includeEpisodes: true,
-    includeNonVideos: true,
-    duplicateCategories: true,
-    outputDir: "/config/xseeds",
-    torrentDir: "/config/qBittorrent/BT_backup",
-    qbittorrentUrl: "http://localhost:8080",
-    rssCadence: "15 minutes", // autobrr doesnt get every announcement
-};
+  action: "inject",
+  matchMode: "safe",
+  skipRecheck: true,
+  includeEpisodes: true,
+  includeNonVideos: true,
+  duplicateCategories: true,
+  outputDir: "/config/xseeds",
+  torrentDir: "/config/qBittorrent/BT_backup",
+  qbittorrentUrl: "http://localhost:8080",
+  rssCadence: "15 minutes", // autobrr doesnt get every announcement
+}
