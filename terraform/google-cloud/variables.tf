4fa7bbdb52d11b284d02011bdf99b2f0f4a51b1f
mchesterbot <202745+mchesterbot[bot]@users.noreply.github.com>
2023-11-18 04:21:28 +0000

    fix(github-release): update dependency louislam/uptime-kuma to v1.23.6

--- terraform/google-cloud/variables.tf
+++ terraform/google-cloud/variables.tf
@@ -32,7 +32,7 @@ variable "tag" {
   type = string
   description = "Uptime Kuma Tag"
   # renovate: datasource=github-releases depName=louislam/uptime-kuma
-  default = "1.23.4"
+  default = "1.23.6"
 }
 variable "zone" {
   type = string
