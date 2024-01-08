77c017b02fb38a93f6706bbf91bde58c42394cfc
Mike Chester <mike@chester.io>
2024-01-07 20:43:13 -0800

    feat: volsync template

---
+++ .taskfiles/_scripts/wait-for-k8s-job.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+JOB_NAME=$1
+NAMESPACE="${2:-default}"
+CLUSTER=$3
+
+[[ -z "${JOB_NAME}" ]] && echo "Job name not specified" && exit 1
+
+while true; do
+    STATUS="$(kubectl --context "${CLUSTER}" -n "${NAMESPACE}" get pod -l job-name="${JOB_NAME}" -o jsonpath='{.items[*].status.phase}')"
+    if [[ "${STATUS}" == "Pending" ]]; then
+        break
+    fi
+    sleep 1
+done
