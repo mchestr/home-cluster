541b7b76503668a1e44c5bc57c5f74fb155b18a9
Mike Chester <mchestr@users.noreply.github.com>
2024-06-04 18:00:39 -0700

    Update waf.tf

--- terraform/cloudflare/waf.tf
+++ terraform/cloudflare/waf.tf
@@ -2,14 +2,14 @@
 resource "cloudflare_filter" "block_countries" {
   zone_id     = data.cloudflare_zone.domain.id
   description = "Expression to block countries"
-  expression  = "(not ip.geoip.country in {\"CA\" \"US\" \"CN\" \"IT\" \"CH\" \"NL\"})"
+  expression  = "(not ip.geoip.country in {\"CA\" \"US\" \"CN\"})"
 }
-#resource "cloudflare_firewall_rule" "block_countries" {
-#  zone_id     = data.cloudflare_zone.domain.id
-#  description = "Firewall rule to block countries"
-#  filter_id   = cloudflare_filter.block_countries.id
-#  action      = "block"
-#}
+resource "cloudflare_firewall_rule" "block_countries" {
+  zone_id     = data.cloudflare_zone.domain.id
+  description = "Firewall rule to block countries"
+  filter_id   = cloudflare_filter.block_countries.id
+  action      = "block"
+}
 
 # Block Bots
 resource "cloudflare_filter" "bots" {
