99f17cd98b5d2a5b327d88f90e2b051ec6ef28a8
Mike Chester <mike@chester.io>
2025-01-19 11:15:40 -0800

    fix: format the python file, we have standards here

--- hack/flux-visualizer.py
+++ hack/flux-visualizer.py
@@ -4,52 +4,63 @@ import pyvis
 from pyvis.network import Network
 import os
 import yaml
+
 pyvis.__version__
 
-nodes = set()
-edges = set()
-for root, dirs, files in os.walk(".."):
-    path = root.split(os.sep)
-    for file in (f for f in files if f.endswith('.yaml')):
-        filepath = os.path.join(root, file)
-        with open(filepath, 'r') as f:
-            data = yaml.load_all(f, yaml.FullLoader)
+
+def extract_file(filepath):
+    nodes = set()
+    edges = set()
+    with open(filepath, "r") as f:
+        data = yaml.load_all(f, yaml.FullLoader)
+        for document in data:
+            if type(document) == list:
+                print(f"invalid file?: {filepath}")
+                continue
+
+            if document.get("kind") == "Kustomization":
+                node = document.get("metadata", {}).get("name")
+                if node is None:
+                    continue
+                nodes.add(node)
+
+                for dep in document.get("spec", {}).get("dependsOn", []):
+                    dep_name = dep.get("name")
+                    edges.add((dep_name, node))
+    return nodes, edges
+
+
+def parse_repository(path="."):
+    nodes = set()
+    edges = set()
+    for root, _, files in os.walk(path):
+        path = root.split(os.sep)
+        for file in (f for f in files if f.endswith(".yaml")):
+            filepath = os.path.join(root, file)
             try:
-              for document in data:
-                  if type(document) == list:
-                      print(f'invalid file?: {filepath}')
-                      continue
-
-                  if document.get('kind') == 'Kustomization':
-                    node = document.get('metadata', {}).get('name')
-                    if node is None:
-                        continue
-
-                    nodes.add(node)
-
-                    for dep in document.get('spec', {}).get('dependsOn', []):
-                        dep_name = dep.get('name')
-                        edges.add((node, dep_name))
+                file_nodes, file_edges = extract_file(filepath)
+                nodes |= file_nodes
+                edges |= file_edges
             except Exception as e:
-                print(filepath)
-                print(e)
-
-
-net = Network(notebook = True, cdn_resources = "remote",
-                bgcolor = "#222222",
-                font_color = "white",
-                height = "750px",
-                width = "100%",
-                select_menu=True,
-                filter_menu=True,
-)
-net.add_nodes(nodes)
-net.add_edges(edges)
-net.show("graph.html")
+                print(f"Error extracting file {filepath}: {e}")
+    return nodes, edges
 
 
 def main():
-    pass
+    nodes, edges = parse_repository()
+    net = Network(
+        notebook=True,
+        cdn_resources="remote",
+        bgcolor="#222222",
+        font_color="white",
+        height="750px",
+        width="100%",
+        select_menu=True,
+        filter_menu=True,
+    )
+    net.add_nodes(nodes)
+    net.add_edges(edges)
+    net.show("graph.html")
 
 
 if __name__ == "__main__":
